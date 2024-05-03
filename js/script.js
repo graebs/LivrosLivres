280c66850844f15a88cb4e2a6eeef68bfa3e4ca6
graebs <gaandeolca@gmail.com>
2024-05-03 20:33:08 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function mudou(){
+   var modo = window.document.getElementById('modo')
+   if ( modo.checked ){
+      window.document.body.style.backgroundColor = "#07011a"
+} else {
+      window.document.body.style.backgroundColor = "#dde2d7"
+}
+}
